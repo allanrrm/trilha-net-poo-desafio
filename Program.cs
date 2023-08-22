cb37b5abc5496dfe68fb1e8a88effa16bb64999e
Allan Rezende <allan.rezende@outlook.com>
2023-08-22 07:32:57 -0300

    Implementando o Programa

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("98542", "11", "456852357159", 128);
+Nokia nokia = new Nokia("33954897528", "S21", "255842154459", 256);
+
+System.Console.WriteLine("Smartphone Iphone");
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+
+System.Console.WriteLine("Smartphone Nokia");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Bing");
+
+
+
